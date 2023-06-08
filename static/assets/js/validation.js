1756a4ec5a4058636d5083ab665b042d548838f6
Nestor Ingles <15877514+Pylos7@users.noreply.github.com>
2023-06-08 18:07:07 -0400

    Minify validation.js

--- static/assets/js/validation.js
+++ static/assets/js/validation.js
@@ -1,13 +1,1 @@
-function validateForm() {
-    var emailInput = document.getElementById('email');
-    var emailValue = emailInput.value.trim();
-    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!emailRegex.test(emailValue)) {
-        alert('Please enter a valid email address.');
-        emailInput.focus();
-        return false;
-    }
-    
-    return true;
-}
+function validateForm(){var e=document.getElementById("email"),a=e.value.trim();return!!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(a)||(alert("Please enter a valid email address."),e.focus(),!1)}
